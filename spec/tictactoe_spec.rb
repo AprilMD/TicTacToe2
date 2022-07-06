cf870c5f7b6d4af0209b9b11dffc04a600399023
April <aprilmdawson@hotmail.com>
2022-07-06 16:14:44 +0100

    add method to take user input for desired move

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -7,3 +7,16 @@ describe 'tictactoe game'
             expect(game.board).to eq([['-','-','-'],['-','-','-'],['-','-','-']])
         end
     end
+
+    describe 'ask player for their input for desired move' do
+        it 'changes the variable row to the given row' do
+            game = TicTacToe.new
+            game.ask_player_move
+            expect(game.row).not_to eql(0)
+        end
+        it 'changes the variable column to the given column' do
+            game = TicTacToe.new
+            game.ask_player_move
+            expect(game.column).not_to eql(0)
+        end
+    end
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,7 +1,18 @@
 class TicTacToe
     attr_accessor :board
-
+    attr_accessor :row
+    attr_accessor :column
     def initialize()
         @board = [["-","-","-"],["-","-","-"],["-","-","-"]]
+        @row = 0
+        @column = 0
+    end
+
+    def ask_player_move
+        puts "What row do you want to place your marker in?"
+        @row = gets.chomp.to_i
+        puts "and your column?"
+        @column = gets.chomp.to_i
+        return @row, @column
     end
 end
