bde9b2a963dfc6c127db39ecabf6d37f036f262a
April <aprilmdawson@hotmail.com>
2022-07-06 23:18:00 +0100

    add play_game file with full game logic

--- play_game.rb
+++ play_game.rb
@@ -6,29 +6,40 @@ def play_game
     game.print_board
     puts "The player to take the first turn will be player 1 and assigned the 'X' symbol"
 
-    game.ask_player_move
-    if game.row_and_column_exist(game.row, game.column)
-        if game.cell_is_available(game.row, game.column)
-            game.determine_player_symbol(game.turn_count)
-            game.execute_move(game.row, game.column, game.player)
-            game.print_board
-            if game.player_won(game.player)
-                puts "Congratulations - #{game.player} wins!"
-            elsif game.board_full
-                puts "Uh-oh, that's a tie."
+    while true
+        game.ask_player_move
+        if game.row_and_column_exist(game.row, game.column)
+            if game.cell_is_available(game.row, game.column)
+                game.determine_player_symbol(game.turn_count)
+                game.execute_move(game.row, game.column, game.player)
+                game.print_board
+                if game.player_won(game.player)
+                    puts "Congratulations - player #{game.player} wins!"
+                    puts "do you want to play again? y/n?"
+                    answer = gets.chomp
+                    if answer.downcase == "y"
+                        play_game
+                    else
+                        break
+                    end
+                elsif game.board_full
+                    puts "Uh-oh, that's a tie."
+                    puts "do you want to play again? y/n?"
+                    answer = gets.chomp
+                    if answer.downcase == "y"
+                        play_game
+                    else
+                        break
+                    end
+                end
             else
-                puts "hey"
-                game.ask_player_move
+                puts "cell is already occupied"
+                game.print_board
+                puts "please choose a cell that is available, marked with a '-'"
             end
         else
-            puts "cell is already occupied"
-            game.print_board
-            puts "please choose a cell that is available, marked with a '-'"
-            ask_player_move
+            puts "invalid coordinates entered; please enter a row and column between 1 and 3."
         end
-    else
-        puts "invalid coordinates entered; please enter a row and column between 1 and 3."
-        game.ask_player_move
     end
 end
 
