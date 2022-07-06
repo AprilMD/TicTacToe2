e2c255e31aae9377766459ae2f1beffd2ed565bf
April <aprilmdawson@hotmail.com>
2022-07-06 15:27:35 +0100

    add game board

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,1 +1,9 @@
-require_relative ../tictactoe.rb
+require_relative '../tictactoe.rb'
+
+describe 'tictactoe game'
+    describe 'tictactoe board' do
+        it 'shows a 3x3 grid' do
+            game = TicTacToe.new
+            expect(game.board).to eq([['-','-','-'],['-','-','-'],['-','-','-']])
+        end
+    end
